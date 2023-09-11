6a354d5d6119fd09ebba2960392d6737fe557a10
Arpita Singh <arpitas@infinite.com>
2023-09-11 10:57:58 +0530

    add

---
+++ C# codebase test/CodeBase/Assignment1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBase
+{
+    class Assignment1
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string inputString = Console.ReadLine();
+
+            Console.Write("Enter the position to remove): ");
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            if (position >= 0 && position < inputString.Length)
+            {
+                string result = inputString.Remove(position, 1);
+                Console.WriteLine("Modified string: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid position. The string remains unchanged");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ C# codebase test/CodeBase/Assignment2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CodeBase
+{
+
+    class Assignment2
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+                
+            {
+                Console.WriteLine(num + "*" + i + "=" + num * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
