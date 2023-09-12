f7e24fb5b7806ee08e47475a1bc34d8750e61542
Arpita Singh <arpitas@infinite.com>
2023-09-12 23:26:18 +0530

    add

---
+++ Assignment_4/Accounts.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Assignment_4
+{
+    class Accounts
+    {
+        private string accountNo;
+        private string customerName;
+        private string accountType;
+        private char transactionType;
+        private decimal amount;
+        private decimal balance;
+
+        public Accounts(string accountNo, string customerName, string accountType, decimal startingBalance)
+        {
+            this.accountNo = accountNo;
+            this.customerName = customerName;
+            this.accountType = accountType;
+            this.balance = startingBalance;
+        }
+
+        public void Credit(decimal amount)
+        {
+            this.balance += amount;
+        }
+
+        public void Debit(decimal amount)
+        {
+            if (amount <= balance)
+            {
+                this.balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("===========================Account Detail================================");
+            Console.WriteLine("Account No: " + accountNo);
+            Console.WriteLine("Customer Name: " + customerName);
+            Console.WriteLine("Account Type: " + accountType);
+            Console.WriteLine("Balance: " + balance);
+            Console.WriteLine("=========================================================================");
+        }
+
+        public void PerformTransaction()
+        {
+            Console.Write("Enter transaction type (D->Deposit, W->Withdrawal): ");
+            char transactionType = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (transactionType == 'D')
+            {
+                Console.Write("Enter the amount to deposit: ");
+                if (decimal.TryParse(Console.ReadLine(), out decimal depositAmount))
+                {
+                    Credit(depositAmount);
+                    Console.WriteLine("Deposit successful.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+            else if (transactionType == 'W')
+            {
+                Console.Write("Enter the amount to withdraw: ");
+                if (decimal.TryParse(Console.ReadLine(), out decimal withdrawalAmount))
+                {
+                    Debit(withdrawalAmount);
+                    Console.WriteLine("Withdrawal successful.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid transaction type.");
+            }
+        }
+    }
+
+    class Programs
+    {
+        static void Main()
+        {
+            Console.Write("Enter Account No: ");
+            string accountNo = Console.ReadLine();
+
+            Console.Write("Enter Customer Name: ");
+            string customerName = Console.ReadLine();
+
+            Console.Write("Enter Account Type: ");
+            string accountType = Console.ReadLine();
+
+            Console.Write("Enter Starting Balance: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal startingBalance))
+            {
+                Accounts account = new Accounts(accountNo, customerName, accountType, startingBalance);
+
+                account.PerformTransaction();
+                account.ShowData();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment_4/student.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Student
+    {
+        private int rollno;
+        private string name;
+        private string class_name;
+        private string semester;
+        private string branch;
+        private int[] marks = new int[5];
+
+        public Student(int rollno, string name, string class_name, string semester, string branch)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.class_name = class_name;
+            this.semester = semester;
+            this.branch = branch;
+        }
+
+        public void GetMarks()
+        {
+            Console.WriteLine("Enter marks of  5 subjects:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Subject {i + 1} Marks: ");
+                if (int.TryParse(Console.ReadLine(), out int subjectMark))
+                {
+                    marks[i] = subjectMark;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                    i--;
+                }
+            }
+        }
+
+        public void DisplayResult()
+        {
+            int totalMarks = 0;
+            foreach (int mark in marks)
+            {
+                totalMarks += mark;
+            }
+            double averageMarks = totalMarks / 5.0;
+            Console.WriteLine($"Average Marks :{averageMarks}");
+            bool failed = false;
+
+            foreach (int mark in marks)
+            {
+                if (mark < 35)
+                {
+                    Console.WriteLine("*==*Result: Failed*==*");
+                    failed = true;
+                    break;
+                }
+            }
+
+            if (!failed)
+            {
+                if (averageMarks < 50)
+                {
+                    Console.WriteLine("*==*Result: Failed*==*");
+                }
+                else
+                {
+                    Console.WriteLine("Result: Passed");
+                }
+            }
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("===============================================");
+            Console.WriteLine("==============Detail of the student============");
+            Console.WriteLine("Roll No: " + rollno);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Class: " + class_name);
+            Console.WriteLine("Semester: " + semester);
+            Console.WriteLine("Branch: " + branch);
+            Console.WriteLine("=====================Marks=====================");
+            Console.WriteLine("Marks: " + string.Join(", ", marks));
+            Console.WriteLine("===================Result======================");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter Roll No: ");
+            int rollno = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Class: ");
+            string class_name = Console.ReadLine();
+
+            Console.Write("Enter Semester: ");
+            string semester = Console.ReadLine();
+
+            Console.Write("Enter Branch: ");
+            string branch = Console.ReadLine();
+
+            Student student = new Student(rollno, name, class_name, semester, branch);
+            student.GetMarks();
+
+            student.DisplayData();
+            student.DisplayResult();
+            Console.Read();
+        }
+        
+    }
+
+}
