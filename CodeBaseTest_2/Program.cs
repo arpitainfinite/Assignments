08258f1a32f8a1d0e322fae8506d660926c75773
Arpita Singh <arpitas@infinite.com>
2023-09-19 10:51:32 +0530

    add

---
+++ CodeBaseTest_2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBaseTest_2
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    class Undergraduate : Student
+    {
+        public Undergraduate(string name, int studentId, double grade)
+        {
+            Name = name;
+            StudentId = studentId;
+            Grade = grade;
+        }
+
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+
+    class Graduate : Student
+    {
+        public Graduate(string name, int studentId, double grade)
+        {
+            Name = name;
+            StudentId = studentId;
+            Grade = grade;
+        }
+
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the name of Undergraduate student: ");
+            string undergradName = Console.ReadLine();
+
+            Console.Write("Enter the student ID of Undergraduate student: ");
+            int undergradId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the grade for Undergraduate student: ");
+            double undergradGrade = Convert.ToDouble(Console.ReadLine());
+
+            Undergraduate undergradStudent = new Undergraduate(undergradName, undergradId, undergradGrade);
+
+            Console.Write("\nEnter the name of Graduate student: ");
+            string gradName = Console.ReadLine();
+
+            Console.Write("Enter the student ID of Graduate student: ");
+            int gradId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the grade for Graduate student: ");
+            double gradGrade = Convert.ToDouble(Console.ReadLine());
+
+            Graduate gradStudent = new Graduate(gradName, gradId, gradGrade);
+
+            Console.WriteLine("\nResults:");
+            Console.WriteLine($"Undergraduate {undergradStudent.Name} (ID: {undergradStudent.StudentId}) Passed: {undergradStudent.IsPassed(undergradStudent.Grade)}");
+            Console.WriteLine($"Graduate {gradStudent.Name} (ID: {gradStudent.StudentId}) Passed: {gradStudent.IsPassed(gradStudent.Grade)}");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ CodeBaseTest_2/Program1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBaseTest_2
+{
+    class Products
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double   ProductPrice { get; set; }
+    }
+
+    class Program1
+    {
+        static void Main()
+        {
+            List<Products> products = new List<Products>();
+
+            // Accept 10 products from the user
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i + 1}:");
+                Console.Write("Product ID: ");
+                int productId = int.Parse(Console.ReadLine());
+                Console.Write("Product Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                Products product = new Products
+                {
+                    ProductId = productId,
+                    ProductName = productName,
+                    ProductPrice = price
+                };
+
+                products.Add(product);
+            }
+
+            products.Sort((p1, p2) => p1.ProductPrice.CompareTo(p2.ProductPrice));
+            Console.WriteLine("==================\nSorted Products based on their Price: ");
+            foreach (Products product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Product Price: {product.ProductPrice:C}");
+                Console.WriteLine("Press Enter ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
