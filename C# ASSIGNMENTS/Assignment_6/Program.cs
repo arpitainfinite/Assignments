bafe52dbc75e913deb52d89eaa74bf6c82a5b7ff
Arpita Singh <arpitas@infinite.com>
2023-09-23 21:21:03 +0530

    add

---
+++ C# ASSIGNMENTS/Assignment_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Ticket Booking System!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                Assignment_6.TicketCalculator.CalculateConcession(age, name);
+            }
+            else
+            {
+                Console.WriteLine("Invalid age input.");
+            }
+        }
+    }
+}
---
+++ C# ASSIGNMENTS/Assignment_6/TicketCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+        public class TicketCalculator
+        {
+            const int TotalFare = 500;
+            public static void CalculateConcession(int age, string name)
+            {
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Age: {age}");
+
+
+
+            if (age <= 5)
+            {
+                Console.WriteLine("Little Champs- Free Ticket");
+            }
+            else if (age > 60)
+            {
+                int fare = (int)(0.7 * TotalFare); // 30% concession for senior citizens
+                Console.WriteLine($"Senior Citizen: {fare}/-");
+            }
+            else
+            {
+                Console.WriteLine($"Ticket Booked: {TotalFare}/-");
+
+            }
+            Console.ReadLine();
+        }
+        }
+    }
