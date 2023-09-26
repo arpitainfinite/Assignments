f7b81f9f6e96e22193a8cd254a6673309c35060f
Arpita Singh <arpitas@infinite.com>
2023-09-26 11:12:36 +0530

    Assignment_7

---
+++ C# ASSIGNMENTS/Assignment_7/RemotingClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using RemotingServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create channel with port
+            HttpChannel c1 = new HttpChannel(8002);
+
+            //register channel
+            ChannelServices.RegisterChannel(c1);
+
+            //create a service class object
+            Service service = (Service)Activator.GetObject(typeof(Service),
+                "http://localhost:85/OurFirstRemoteService");
+
+            //"tcp://localhost:8089/OurFirstRemoteService"
+            //start calling the functions of the service class
+            Console.WriteLine(service.SayHello("  Remote"));
+            
+            Console.Read();
+        }
+    }
+}
---
+++ C# ASSIGNMENTS/Assignment_7/RemotingServer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using System.Runtime.Remoting;
+
+using System.Runtime.Remoting.Channels;
+
+using System.Runtime.Remoting.Channels.Http;
+
+using System.Runtime.Remoting.Channels.Tcp;
+
+using System.Windows.Forms;
+
+namespace RemotingServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //create a new channel for communication
+
+            HttpChannel c = new HttpChannel(85);
+
+            // TcpChannel tc = new TcpChannel(8080);
+
+
+
+            //register the channel
+
+            ChannelServices.RegisterChannel(c);
+
+
+
+            //configure all known services
+
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Service), "OurFirstRemoteService", WellKnownObjectMode.Singleton);
+
+            MessageBox.Show("Server Services started at Port No: 85...");
+
+            MessageBox.Show("Press any Key to Stop the Server Services..");
+
+            Console.ReadLine();
+
+        }
+    }
+    public class Service : MarshalByRefObject
+
+    {
+
+        public string SayHello(string s)
+
+        {
+
+            return "Hello" + s;
+
+        }
+
+
+
+    }
+}
