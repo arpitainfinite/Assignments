36493558d1ca8916b59c3bb4e470da963eddd789
Arpita Singh <arpitas@infinite.com>
2023-10-31 23:38:35 +0530

    web assignment

---
+++ Web Technology/Web Assignment/WebAssignment/Form1.aspx
@@ -0,0 +1,36 @@
+﻿﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form1.aspx.cs" Inherits="WebAssignment.Form1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Store Application</title>
+    <style>
+        h1{
+            text-align:center;
+        }
+        .images{
+           margin-left:15ex;
+        }
+        .Itemslist{
+            margin-top:-10ex;
+     
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1> Welcome to Electronics Store</h1>
+        <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+
+        <asp:DropDownList ID="Itemslist" Class="Itemslist" runat="server" AutoPostBack="true" OnSelectedIndexChanged="Itemslist_SelectedIndexChanged">
+            <asp:ListItem Text="Select an item" Value="default.jpg" />
+            <asp:ListItem Text="SmartPhones" Value="smartphone.jpg" />
+            <asp:ListItem Text="Laptops" Value="laptop.jpeg"/>
+            <asp:ListItem Text="Headphones" Value="headphones.jpg" />
+            <asp:ListItem Text="Earbuds" Value="earbuds.jpg" />
+        </asp:DropDownList>
+        <asp:Image ID="imgItem" Class="images" runat="server" Width="300px" Height="300px"/>
+        <asp:Label ID="lblCost" Class="lblCost" runat="server" Text="" />
+    </form>
+</body>
+</html>
---
+++ Web Technology/Web Assignment/WebAssignment/Form1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace WebAssignment
+{
+    public partial class Form1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set the initial selection of the dropdown list
+                Itemslist.SelectedIndex = 0;
+                // Display the default image (you can set this in the markup as well)
+                imgItem.ImageUrl = "Img/main.jfif";
+            }
+        }
+
+        protected void Itemslist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Set the image URL based on the selected item
+            string selectedImageUrl = Itemslist.SelectedValue;
+            imgItem.ImageUrl = $"Img/{selectedImageUrl}";
+            lblCost.Text = ""; // Clear the cost label.
+        }
+
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            string selectedItem =Itemslist.SelectedValue;
+
+            // Define a dictionary of item costs (you can use a database in a real application).
+            var itemCosts = new Dictionary<string, decimal>
+            {
+                { "smartphone.jpg", 20000.00m },
+                { "laptops.jpeg", 50000.00m },
+                { "headphones.jpg", 10000.00m },
+                { "earbuds.jpg", 12900.00m },
+            };
+
+            if (itemCosts.ContainsKey(selectedItem))
+            {
+                lblCost.Text = $"Cost: ₹{itemCosts[selectedItem]}";
+            }
+            else
+            {
+                lblCost.Text = "Cost not available.";
+            }
+        }
+    }
+}
